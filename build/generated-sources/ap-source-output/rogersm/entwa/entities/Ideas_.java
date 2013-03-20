52d1dd607844ef2b2a41471d38a8a656eb3b97de
Mike Rogers <mike.r@fullondesign.co.uk>
2013-03-20 13:59:26 +0000

    updating java docs

--- build/generated-sources/ap-source-output/rogersm/entwa/entities/Ideas_.java
+++ build/generated-sources/ap-source-output/rogersm/entwa/entities/Ideas_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import rogersm.entwa.entities.People;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-03-18T13:53:30")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-03-20T13:58:53")
 @StaticMetamodel(Ideas.class)
 public class Ideas_ { 
 
