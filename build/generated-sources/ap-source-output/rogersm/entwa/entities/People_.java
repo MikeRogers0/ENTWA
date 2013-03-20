893d697f678958a3affac37108015e937c9776a9
Mike Rogers <mike.r@fullondesign.co.uk>
2013-03-20 14:01:51 +0000

    updating app strucutre to not use faces

--- build/generated-sources/ap-source-output/rogersm/entwa/entities/People_.java
+++ build/generated-sources/ap-source-output/rogersm/entwa/entities/People_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import rogersm.entwa.entities.Ideas;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-03-20T13:58:53")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-03-20T14:00:16")
 @StaticMetamodel(People.class)
 public class People_ { 
 
